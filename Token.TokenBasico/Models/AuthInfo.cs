31eecc1e60ad76b2a2f137294dc798e101e89236
luiszbRTA <luis.zuniga@rtasoftware.com.mx>
2021-09-08 16:08:27 -0500

    Agregar modelo de configuración para crear jwt token. Agregar modelo de authenticación.

---
+++ Token.TokenBasico/Models/AuthInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Token.TokenBasico.Models
+{
+    public class AuthInfo
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
