a4f822794a1ff99c7581362f61076847030b4a52
luiszbRTA <luis.zuniga@rtasoftware.com.mx>
2021-09-09 18:40:57 -0500

    Agregar Configuración de base de datos

--- Token.TokenLocalAuth/Startup.cs
+++ Token.TokenLocalAuth/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Token.TokenLocalAuth.Data;
 
 namespace Token.TokenLocalAuth
 {
@@ -23,6 +25,12 @@ namespace Token.TokenLocalAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(
+            Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<ApplicationDbContext>();
+
             services.AddRazorPages();
         }
 
