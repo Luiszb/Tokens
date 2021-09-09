5fdbaef3f06fc80fc5ada125b6afbff93e4049d5
luiszbRTA <luis.zuniga@rtasoftware.com.mx>
2021-09-09 18:50:49 -0500

    Registrar middleware de authenticación

--- Token.TokenLocalAuth/Startup.cs
+++ Token.TokenLocalAuth/Startup.cs
@@ -62,7 +62,7 @@ namespace Token.TokenLocalAuth
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
